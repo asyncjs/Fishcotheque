b510e83b06d72ae3f6c5e4f84b48bc69e0f69a71
Matthew McCorry <mccorry@gmail.com>
2016-09-08 20:39:20 +0100

    added crab

--- javascript/creatures.js
+++ javascript/creatures.js
@@ -12,7 +12,8 @@
         // CREATURES
         "creatures/example-fish-1/example-fish-1.js",
         "creatures/example-fish-2/example-fish-2.js",
-        "creatures/link-fish/link-fish.js"
+        "creatures/link-fish/link-fish.js",
+		"creatures/ms-crab/ms-crab.js"
 
     );
 }());
