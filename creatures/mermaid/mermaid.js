dfe5e259a27f33b5cfe066915b8af3f84ff324d1
Yann Eves <hello@yanneves.com>
2016-09-08 21:42:52 +0100

    Mermaid executes Link

--- creatures/mermaid/mermaid.js
+++ creatures/mermaid/mermaid.js
@@ -35,6 +35,18 @@ fishcotheque.createCreature('best-mermaid', function (creature) {
   // Load external CSS file
   fishcotheque.loadCSS('creatures/mermaid/mermaid.css');
 
+  // Link execution
+  setTimeout(function() {
+    var $ = fishcotheque.jQuery
+    var link = $.find('[data-id="link-fish"]');
+
+    if (link && link.length) {
+      link = $(link[0]);
+      drawLineAndDestroyAfter2Seconds($, creature.position(), link.position());
+      link.hide();
+    }
+  }, 3000);
+
   // Update the creature's state every time we redraw the fishcotheque
   fishcotheque.bind('tick', function() {
     var down = (rnd(0, velocity) * speedVector.y)
@@ -79,6 +91,33 @@ fishcotheque.createCreature('best-mermaid', function (creature) {
     creature.position(newpos);
   });
 });
+
+function drawLineAndDestroyAfter2Seconds($, origin, target) {
+  var canvasSize = fishcotheque.size();
+
+  var svg = $(document.createElementNS('http://www.w3.org/2000/svg', 'svg'));
+  var line = $(document.createElementNS('http://www.w3.org/2000/svg', 'line'));
+
+  svg.attr('version', '1.1');
+  svg.attr('width', canvasSize.width - 1);
+  svg.attr('height', canvasSize.height - 1);
+  svg.attr('viewport', '0 0 120 120');
+
+  line.attr('id','zap');
+  line.attr('x1',origin.left+80);
+  line.attr('y1',origin.top+80);
+  line.attr('x2',target.left+40);
+  line.attr('y2',target.top+40);
+  line.attr('stroke','purple');
+  line.attr('stroke-width','2');
+
+  svg.append(line);
+  $("body").append(svg);
+
+  setTimeout(function () {
+    svg.remove()
+  }, 1200);
+}
 
 // Generate a random number between two values
 function rnd(lower, upper) {
